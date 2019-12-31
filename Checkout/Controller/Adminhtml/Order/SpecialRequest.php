293f45cdd2d721481bf20fb476fb34a7fbdd0238
Ongnoiban2010 <48355044+Ongnoiban2010@users.noreply.github.com>
2019-12-31 14:01:53 +0700

    Add files via upload

---
+++ Checkout/Controller/Adminhtml/Order/SpecialRequest.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace DTN\Checkout\Controller\Adminhtml\Order;
+
+use Magento\Backend\App\Action;
+use Magento\Sales\Api\OrderManagementInterface;
+use Magento\Sales\Api\OrderRepositoryInterface;
+use Psr\Log\LoggerInterface;
+
+class SpecialRequest extends \Magento\Sales\Controller\Adminhtml\Order
+{
+    /**
+     * @var \Magento\Framework\View\LayoutFactory
+     */
+    protected $layoutFactory;
+
+    public function __construct(
+        Action\Context $context,
+        \Magento\Framework\Registry $coreRegistry,
+        \Magento\Framework\App\Response\Http\FileFactory $fileFactory,
+        \Magento\Framework\Translate\InlineInterface $translateInline,
+        \Magento\Framework\View\Result\PageFactory $resultPageFactory,
+        \Magento\Framework\Controller\Result\JsonFactory $resultJsonFactory,
+        \Magento\Framework\View\Result\LayoutFactory $resultLayoutFactory,
+        \Magento\Framework\Controller\Result\RawFactory $resultRawFactory,
+        OrderManagementInterface $orderManagement,
+        OrderRepositoryInterface $orderRepository,
+        LoggerInterface $logger,
+        \Magento\Framework\View\LayoutFactory $layoutFactory
+    ) {
+        $this->layoutFactory = $layoutFactory;
+        parent::__construct(
+            $context,
+            $coreRegistry,
+            $fileFactory,
+            $translateInline,
+            $resultPageFactory,
+            $resultJsonFactory,
+            $resultLayoutFactory,
+            $resultRawFactory,
+            $orderManagement,
+            $orderRepository,
+            $logger
+        );
+    }
+
+    public function execute()
+    {
+        $this->_initOrder();
+        $layout = $this->layoutFactory->create();
+        // Yes, this is the same block class that we defined in sales_order_view.xml
+        $html = $layout->createBlock('DTN\Checkout\Block\Adminhtml\Order\SpecialRequest')
+            ->toHtml();
+        $this->_translateInline->processResponseBody($html);
+        /** @var \Magento\Framework\Controller\Result\Raw $resultRaw */
+        $resultRaw = $this->resultRawFactory->create();
+        $resultRaw->setContents($html);
+        return $resultRaw;
+    }
+}
